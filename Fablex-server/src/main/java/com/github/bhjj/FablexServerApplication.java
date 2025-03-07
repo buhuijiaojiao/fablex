f0a5a47ebbdb06a07b07188b9ffe41942439a8ae
bhjj <3317038611@qq.com>
2025-03-08 00:00:51 +0800

    项目基本框架搭建

---
+++ Fablex-server/src/main/java/com/github/bhjj/FablexServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.bhjj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FablexServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FablexServerApplication.class, args);
+    }
+
+}
---
+++ Fablex-server/src/test/java/com/github/bhjj/FablexServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bhjj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FablexServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

76da7b008d5707dd4ce0083ffaea5ed952cbd740
bhjj <3317038611@qq.com>
2025-03-08 15:52:32 +0800

    基本包结构创建

--- Fablex-server/src/main/java/com/github/bhjj/FablexServerApplication.java
+++ Fablex-server/src/main/java/com/github/bhjj/FablexServerApplication.java
@@ -3,11 +3,13 @@ package com.github.bhjj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author bhjj
+ * @Date 2025/3/8
+ */
 @SpringBootApplication
 public class FablexServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FablexServerApplication.class, args);
     }
-
 }
